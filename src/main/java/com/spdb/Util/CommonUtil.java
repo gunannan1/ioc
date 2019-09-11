66f61139a02167bfc53d594d74c65aaf08b2787f
yuxudong1512025 <1161431707@qq.com>
2019-09-11 14:49:12 +0800

    修改非空判断

---
+++ src/main/java/com/spdb/Util/CommonUtil.java
@@ -0,0 +1,42 @@
+package com.spdb.Util;
+import	java.util.Set;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CommonUtil {
+
+    private CommonUtil(){}
+
+    public static boolean isNullOrEmpty(Object obj){
+        if (obj instanceof Object []){
+            Object [] arr = (Object [])obj;
+            if (arr.length ==0){
+                return false;
+            }return true;
+        }else if (obj instanceof String) {
+            String str = (String)obj;
+            if (str.length()<=0||str==null){
+                return false;
+            }return true;
+        }else if (obj instanceof List){
+            List list = (List)obj;
+            if (list==null ||list.isEmpty()){
+                return false;
+            }return true;
+        }else if (obj instanceof Map){
+            Map map = (Map)obj;
+            if (map== null || map.isEmpty()){
+                return false;
+            }return true;
+        }else if (obj instanceof Set){
+            Set set = (Set)obj;
+            if (set == null || set.isEmpty()){
+                return false;
+            }return true;
+        }else{
+            return obj==null?false:true;
+        }
+    }
+}
--- src/main/java/com/spdb/Util/Valiator/NotNull/NotNull.java
+++
@@ -1,9 +0,0 @@
-package com.spdb.Util.Valiator.NotNull;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface NotNull {
-}
