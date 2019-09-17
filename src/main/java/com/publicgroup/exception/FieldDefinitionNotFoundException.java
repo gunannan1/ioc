810b641e6155136c7d396b05ad15fd6b6b7a4a0a
yuxudong1512025 <1161431707@qq.com>
2019-09-17 09:54:09 +0800

    修改ref引用在xml中的形式

---
+++ src/main/java/com/publicgroup/exception/FieldDefinitionNotFoundException.java
@@ -0,0 +1,10 @@
+package com.publicgroup.exception;
+
+public class FieldDefinitionNotFoundException extends BeanException {
+    /**  */
+    private static final long serialVersionUID = 1L;
+
+    public FieldDefinitionNotFoundException(String msg) {
+        super(msg);
+    }
+}
